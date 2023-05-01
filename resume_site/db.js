1d5aaa6e33057b5c44b1717afa8ce9890f2955d8
Bryan "Ceres <fallacybv@gmail.com>
2023-05-01 14:42:26 -0700

    Add files via upload

---
+++ resume_site/db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2")
+
+var db = mysql.createConnection({
+
+    host: 'localhost',
+    database: "resume",
+    user: 'root',
+    password: 'password'
+})
+
+module.exports = db;
